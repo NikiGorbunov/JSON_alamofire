6f5e3db8484b27224edc1e1a1e745aa490b8b739
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-03-17 15:47:38 +0300

    work 2.10

--- JSON_alamofire/ViewControllers/RecipeViewController.swift
+++ JSON_alamofire/ViewControllers/RecipeViewController.swift
@@ -12,7 +12,6 @@ class RecipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 }
